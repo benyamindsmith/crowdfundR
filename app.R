be6e0427a24e9ae23dc69b1bc69cf671ac626340
bsmithClearSum <46410142+benyamindsmith@users.noreply.github.com>
2023-03-25 23:21:51 -0400

    still a skeleton

--- app.R
+++ app.R
@@ -3,99 +3,62 @@ library(brochure)
 library(shiny)
 
 # Creating a navlink
-nav_links <- tags$ul(
+admin_nav <- tags$ul(
   tags$li(
-    tags$a(href = "/", "home"),
+    tags$a(href = "/admin", "home"),
   ),
   tags$li(
-    tags$a(href = "/login", "login"),
+    tags$a(href = "/admin/graphics", "add graphics"),
   ),
   tags$li(
-    tags$a(href = "/logout", "logout"),
+    tags$a(href = "/admin/payments-pledges", "manage payments/pledges"),
   )
 )
 
 home <- function() {
   page(
     href = "/",
-    ui = tagList(
-      h1("This is my first page"),
-      tags$p("It will contain BROCHURECOOKIE depending on the last page you've visited (/login or /logout)"),
-      verbatimTextOutput("cookie"),
-      nav_links
-    ),
-    server = function(input, output, session) {
-      output$cookie <- renderPrint({
-        parse_cookie_string(
-          get_cookies()
-        )
-      })
-    }
+    ui = tagList(),
+    server = function(input, output, session) {}
   )
 }
 
-login <- function() {
+admin_home <- function() {
   page(
-    href = "/login",
+    href = "/admin",
     ui = tagList(
-      h1("You've just logged!"),
-      verbatimTextOutput("cookie"),
-      actionButton("redirect", "Redirect to the home page"),
-      nav_links
+      admin_nav
     ),
-    server = function(input, output, session) {
-      output$cookie <- renderPrint({
-        parse_cookie_string(
-          get_cookies()
-        )
-      })
-      observeEvent(input$redirect, {
-        # Using brochure to redirect to another page
-        server_redirect("/")
-      })
-    },
-    res_handlers = list(
-      # We'll add a cookie here
-      ~ set_cookie(.x, "BROCHURECOOKIE", 12)
-      # If you had to do it yourself
-      # function(res, req){
-      #   res$headers$`Set-Cookie` <- "BROCHURECOOKIE=12; HttpOnly;"
-      #   res
-      # }
-    )
+    server = function(input, output, session) {},
+    res_handlers = list()
   )
 }
 
-logout <- function() {
+admin_graphics <- function(){
   page(
-    href = "/logout",
+    href = "/admin/graphics",
     ui = tagList(
-      h1("You've logged out"),
-      nav_links,
-      verbatimTextOutput("cookie")
+      admin_nav
     ),
-    server = function(input, output, session) {
-      output$cookie <- renderPrint({
-        parse_cookie_string(
-          get_cookies()
-        )
-      })
-    },
-    res_handlers = list(
-      # We'll remove the cookie here
-      ~ remove_cookie(.x, "BROCHURECOOKIE")
-      # If you had to do it yourself
-      # function(res, req){
-      #   res$headers$`Set-Cookie` <- "BROCHURECOOKIE=''; Max-Age = 0;"
-      #   res
-      # }
-    )
+    server = function(input, output, session) {},
+    res_handlers = list()
   )
 }
 
+admin_payment_pledges <- function(){
+  page(
+    href = "/admin/payments-pledges",
+    ui = tagList(
+      admin_nav
+    ),
+    server = function(input, output, session) {},
+    res_handlers = list()
+  )
+}
 brochureApp(
   # Pages
   home(),
-  login(),
-  logout()
+  admin_home(),
+  admin_graphics(),
+  admin_payment_pledges()
 )
